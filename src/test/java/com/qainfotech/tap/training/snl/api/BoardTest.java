d26090edd1adc8a61d4f67bf995427d10089b139
shubhamkatiyarqait <shubhamkatiyar@qainfotech.com>
2017-07-04 12:06:01 +0530

    Update BoardTest.java

--- src/test/java/com/qainfotech/tap/training/snl/api/BoardTest.java
+++ src/test/java/com/qainfotech/tap/training/snl/api/BoardTest.java
@@ -34,6 +34,15 @@ public class BoardTest {
 		System.out.println(board);
 		board.registerPlayer("sumit");
 	}
+	
+	@Test	
+	public void Check_Constructor_Board() throws IOException{
+		JSONObject  data1 = board.data;
+		Board newtest= new Board(board.uuid);
+		JSONObject data2 = newtest.data;
+		assertNotEquals(data1,data2);
+
+	}
 
 
 	@Test(expectedExceptions=MaxPlayersReachedExeption.class)
@@ -79,7 +88,52 @@ public class BoardTest {
 		board.rollDice((UUID) ((JSONObject) board.getData().getJSONArray("players").get(1)).get("uuid"));
 		
 	}
+	@Test
+	public void players_Turn()throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		board.rollDice((UUID) ((JSONObject) board.getData().getJSONArray("players").get(0)).get("uuid"));
+		board.rollDice((UUID) ((JSONObject) board.getData().getJSONArray("players").get(1)).get("uuid"));
+		assertThat(board.getData().get("turn")).isEqualTo(0);
+	}
 	
+	@Test
+	public void Check_New_Position()
+			throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException,
+			MaxPlayersReachedExeption, IOException, InvalidTurnException, NoUserWithSuchUUIDException {
+		((JSONObject) board.getData().getJSONArray("players").get(0)).put("position", 100);
+		Object d = ((JSONObject) board.getData().getJSONArray("players").get(0)).getInt("position");
+		UUID uuid = (UUID) ((JSONObject) board.getData().getJSONArray("players").get(0)).get("uuid");
+		Object obj = board.rollDice(uuid);
+		Object msg = ((JSONObject) obj).get("message");
+		if ((int) d >= 100) {
+			assertThat(msg.toString()).isEqualTo("Incorrect roll of dice. Player did not move");
+}
+	}
+	
+	@Test
+	public void rollDice_is_valid() throws FileNotFoundException, UnsupportedEncodingException, PlayerExistsException, GameInProgressException, MaxPlayersReachedExeption, IOException, JSONException, InvalidTurnException {
+
+		UUID uuid = (UUID) board.getData().getJSONArray("players").getJSONObject(0).get("uuid");
+		Integer pos = (Integer) board.data.getJSONArray("players").getJSONObject(0).get("position");
+		Integer dice = board.rollDice(uuid).getInt("dice");
+		Integer expected_pos = pos+dice;
+		Integer target = (Integer) board.data.getJSONArray("steps").getJSONObject(dice).get("target");
+		assertTrue(dice<=6);
+		Integer type = (Integer) board.data.getJSONArray("steps").getJSONObject(dice).get("type");
+		if(type==0)
+		{
+			assertEquals( expected_pos, target);
+		}
+		if(type==1)
+		{
+			assertTrue( expected_pos>target);
+		}
+		if(type==2)
+		{
+			assertTrue( expected_pos<target);
+		}
+
+	}
 	
 
 
